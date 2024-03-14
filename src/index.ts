a4661db693287a16168262eae6fee8bbbbf086ff
Carlos Cuesta <carlosmiguel7cb@gmail.com>
2024-03-14 14:37:37 -0500

    without cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors, { type CorsOptions } from 'cors';
+// import cors, { type CorsOptions } from 'cors';
 import conectarDB from './config/db';
 import userRoutes from './routes/user.routes';
 import clientRoutes from './routes/client.routes';
@@ -21,28 +21,33 @@ conectarDB()
     console.error(`Error connecting to the database: ${error.message}.`);
   });
 
-const whiteList: string[] = [];
+// const whiteList: string[] = [];
 
-const frontendUrl = process.env.FRONTEND_URL;
+// const frontendUrl = process.env.FRONTEND_URL;
 
-if (frontendUrl != null) {
-  whiteList.push(frontendUrl);
-}
+// if (frontendUrl != null) {
+//   whiteList.push(frontendUrl);
+// }
 
-const corsOptions: CorsOptions = {
-  origin: function (origin, callback) {
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions, @typescript-eslint/no-unsafe-argument
-    if (whiteList.includes(origin ?? '')) {
-      callback(null, true);
-    } else {
-      callback(new Error('Error de Cors'));
-    }
-  }
-};
+// const whiteList: string[] = [process.env.FRONTEND_URL].filter(
+//   Boolean
+// ) as string[];
 
-app.use(cors(corsOptions));
+// const corsOptions: CorsOptions = {
+//   origin: function (origin, callback) {
+//     // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions, @typescript-eslint/no-unsafe-argument
+//     if (whiteList.includes(origin ?? '')) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Error de Cors'));
+//     }
+//   }
+// };
+
+// app.use(cors(corsOptions));
 
 // Routing
+app.use('/', (req, res) => res.send('hello world'));
 app.use('/api/users', userRoutes);
 app.use('/api/clients', clientRoutes);
 app.use('/api/purchases', purchaseRoutes);
